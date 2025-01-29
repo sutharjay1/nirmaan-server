17d2779871393bc5efc9e6b4495d943421813f5c
sutharjay1 <sutharjay3635@gmail.com>
2025-01-29 22:56:37 +0530

    feat(app): Added CORS

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import express from "express";
-import { urlencoded } from "express";
-import routes from "./route.js";
-import { connectToDB } from "./connecToDB.js";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import { urlencoded } from 'express';
+import routes from './route.js';
+import { connectToDB } from './connecToDB.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(
+	cors({
+		origin: '*',
+		credentials: true,
+	})
+);
 
 connectToDB();
 
@@ -13,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", routes);
+app.use('/api', routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
